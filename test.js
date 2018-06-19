20fe74a592c924c1bf08a3687f05bfddf48b98a5
Gabriel Toll Stålbom <gabtost@gmail.com>
2018-06-19 15:52:24 +0200

    Restructure tests

--- test.js
+++ test.js
@@ -3,8 +3,10 @@ const _get = require('lodash.get');
 const stringify = require('./');
 
 describe('stringify-object-keys', () => {
-  it('handles invalid parameter correctly', () => {
+  it('handles invalid parameter', () => {
     assert.deepEqual(stringify(), []);
+    assert.deepEqual(stringify({}), []);
+    assert.deepEqual(stringify([]), []);
     assert.deepEqual(stringify(undefined), []);
     assert.deepEqual(stringify(null), []);
     assert.deepEqual(stringify(false), []);
@@ -14,54 +16,60 @@ describe('stringify-object-keys', () => {
     assert.deepEqual(stringify(function() {}), []);
   });
 
-  describe('valid object parameter', () => {
-    it('handles objects correctly', () => {
+  describe('object parameter', () => {
+    it('handles objects', () => {
       const obj = { a: 'a', b: { c: 'c', d: { e: { f: 'f' } } } };
       const expected = ['a', 'b.c', 'b.d.e.f'];
 
       assert.deepEqual(stringify(obj), expected);
     });
 
-    it('handles object keys with dots correctly', () => {
+    it('handles object keys with dots', () => {
       const obj = { a: { 'b.b': { c: 'c' } } };
       const expected = ['a[\'b.b\'].c'];
 
       assert.deepEqual(stringify(obj), expected);
     });
 
-    it('handles empty objects correctly', () => {
+    it('handles empty objects', () => {
       const obj = [1, {}, 2]
       const expected = ['[0]', '[1]', '[2]'];
 
       assert.deepEqual(stringify(obj), expected);
     });
+  });
 
-    it('handles arrays correctly', () => {
+  describe('array parameter', () => {
+    it('handles arrays', () => {
       const obj = ['a', [ 'b', 'c', [ 'd' ] ]]
       const expected = ['[0]', '[1][0]', '[1][1]', '[1][2][0]'];
 
       assert.deepEqual(stringify(obj), expected);
     });
 
-    it('handles empty arrays correctly', () => {
+    it('handles empty arrays', () => {
       const obj = [1, [], 2]
       const expected = ['[0]', '[1]', '[2]'];
 
       assert.deepEqual(stringify(obj), expected);
     });
+  });
 
-    it('handles mixed objects and arrays correctly', () => {
+  describe('mixed object/array parameter', () => {
+    it('handles mixed object/array', () => {
       const obj = ['a', { b: 'b', c: [ 'c', { 'd.d': 'd' } ] }]
       const expected = ['[0]', '[1].b', '[1].c[0]', '[1].c[1][\'d.d\']'];
 
       assert.deepEqual(stringify(obj), expected);
     });
+  });
 
-    it('handles lodash.get correctly', () => {
-      const obj = [1, { a: [ { 'b.c.d': ['e'] } ] }, 3];
+  describe('usage with other projects', () => {
+    it('handles lodash.get', () => {
+      const obj = [1, { a: [ { 'b.c.d': [{}] } ] }, 3];
       const keys = stringify(obj);
 
-      assert.deepEqual(_get(obj, keys[1]), 'e');
+      assert.deepEqual(_get(obj, keys[1]), {});
     });
   });
 });
